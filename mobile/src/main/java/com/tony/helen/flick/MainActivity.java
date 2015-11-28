75ce25c7e02a9adf576d6965e2667588435cfd43
tonyli <MingLei.Li@td.com>
2015-11-28 00:49:32 -0500

    Added Basic Myo functionality

--- mobile/src/main/java/com/tony/helen/flick/MainActivity.java
+++ mobile/src/main/java/com/tony/helen/flick/MainActivity.java
@@ -1,16 +1,113 @@
 package com.tony.helen.flick;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.thalmic.myo.AbstractDeviceListener;
+import com.thalmic.myo.DeviceListener;
+import com.thalmic.myo.Hub;
+import com.thalmic.myo.Myo;
+import com.thalmic.myo.Pose;
+import com.thalmic.myo.scanner.ScanActivity;
 
 public class MainActivity extends Activity {
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+
+    private DeviceListener mListener = new AbstractDeviceListener() {
+        @Override
+        public void onConnect(Myo myo, long timestamp) {
+            Log.d("myo", "connected");
+        }
+
+        @Override
+        public void onDisconnect(Myo myo, long timestamp) {
+            Log.d("myo", "Myo Disconnected!");
+        }
+
+        @Override
+        public void onPose(Myo myo, long timestamp, Pose pose) {
+            Log.d("myo", "Pose: " + pose);
+
+            //TODO: Do something awesome.
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Hub hub = Hub.getInstance();
+        if (!hub.init(this)) {
+            Log.e("myo", "Could not initialize the Hub.");
+            finish();
+            return;
+        }
+
+        hub.addListener(mListener);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("This app needs location access");
+                builder.setMessage("Please grant location access so this app can detect beacons.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                    }
+                });
+                builder.show();
+            } else {
+                getMyo();
+            }
+        } else {
+            getMyo();
+        }
+    }
+
+    public void getMyo() {
+        Intent intent = new Intent(this, ScanActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("PERMISSION", "coarse location permission granted");
+                    getMyo();
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+
+                    });
+                    builder.show();
+                }
+                return;
+            }
+        }
     }
 
     @Override
