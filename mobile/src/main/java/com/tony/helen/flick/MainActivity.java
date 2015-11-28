1d4f8b88f87cb67f138109dce52a976e5f7a3788
helenpark <helen2005@rogers.com>
2015-11-28 00:51:06 -0500

    text to speech engine basic install

--- mobile/src/main/java/com/tony/helen/flick/MainActivity.java
+++ mobile/src/main/java/com/tony/helen/flick/MainActivity.java
@@ -1,16 +1,67 @@
 package com.tony.helen.flick;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends Activity {
+
+    // External Communication
+    private int checkEngineStatus;
+
+    // Activity Layout
+    TextToSpeech textEngine;
+
+    Button speech_btn;
+
+    String myoInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+//        Intent checkIntent = new Intent();
+//        checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+//        startActivityForResult(checkIntent, checkEngineStatus);
+
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        myoInput = "hello world";
+
+        speech_btn = (Button) findViewById(R.id.speak_Btn);
+
+        textEngine = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    textEngine.setLanguage(Locale.CANADA);
+                }
+            }
+        });
+
+        speech_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), myoInput, Toast.LENGTH_SHORT).show();
+                textEngine.speak(myoInput, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
     }
 
     @Override
@@ -19,6 +70,8 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
