85343ef2164ecbbaa6d2ec2b145abb41f62b199f
helenpark <helen2005@rogers.com>
2015-11-28 23:14:23 -0500

    Settings paged formatted

--- mobile/src/main/java/com/tony/helen/flick/SettingsActivity.java
+++ mobile/src/main/java/com/tony/helen/flick/SettingsActivity.java
@@ -9,17 +9,39 @@ import android.widget.TextView;
 
 public class SettingsActivity extends Activity {
 
+    TextView phrase_tv1_1;
+    TextView phrase_tv1_2;
+    TextView phrase_tv1_3;
+    TextView phrase_tv1_4;
+
+    TextView phrase_tv2_1;
+    TextView phrase_tv2_2;
+    TextView phrase_tv2_3;
+    TextView phrase_tv2_4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        phrase_tv1_1 = (TextView) findViewById(R.id.phrase_tv1_1);
+        phrase_tv1_2 = (TextView) findViewById(R.id.phrase_tv1_2);
+        phrase_tv1_3 = (TextView) findViewById(R.id.phrase_tv1_3);
+        phrase_tv1_4 = (TextView) findViewById(R.id.phrase_tv1_4);
+
+        phrase_tv2_1 = (TextView) findViewById(R.id.phrase_tv2_1);
+        phrase_tv2_2 = (TextView) findViewById(R.id.phrase_tv2_2);
+        phrase_tv2_3 = (TextView) findViewById(R.id.phrase_tv2_3);
+        phrase_tv2_4 = (TextView) findViewById(R.id.phrase_tv2_4);
+
+
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        getActionBar().hide();
 
 
     }
