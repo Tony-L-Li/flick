07cad1260ee61938b8b43ba98673555585bf3679
helenpark <helen2005@rogers.com>
2015-11-28 19:04:40 -0500

    update android manifest

--- mobile/src/main/java/com/tony/helen/flick/SettingsActivity.java
+++ mobile/src/main/java/com/tony/helen/flick/SettingsActivity.java
@@ -1,7 +1,11 @@
 package com.tony.helen.flick;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SettingsActivity extends Activity {
 
@@ -9,6 +13,15 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        getActionBar().hide();
+
+
     }
 
 }
